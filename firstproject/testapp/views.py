b496a2b2bb7b11ccd8e3c0e66a1fd214dc970eb6
Mahesh <qa.maheshdasari@gmail.com>
2023-05-17 11:53:04 +0530

    first commit by mahesh

--- firstproject/testapp/views.py
+++ firstproject/testapp/views.py
@@ -2,6 +2,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def display(request):
-    print(type(request))
+    print("HI")
     s="<h1>Don't feel difficult...Just it is first time feeling....Really django is very easy....</h1>"
     return HttpResponse(s)
